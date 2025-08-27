47245ed5859ab4aaddfb6d19dd54c1221c53654b
cadullira <cadullira@gmail.com>
2025-08-27 14:11:51 -0300

    feat(api): modulariza conexão com banco de dados
    Move a lógica de conexão do Mongoose para um módulo dedicado `database.js`. Isso limpa o arquivo principal `app.js` e melhora a organização do código, seguindo o princípio de responsabilidade única.

--- api/app.js
+++ api/app.js
@@ -6,14 +6,15 @@ const mongoose = require('mongoose');
 app.use(express.json());
 
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true,});
+/*mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true,});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erro de conexão:'));
 db.once('open', () => {
   console.log('Conexão com o banco de dados estabelecida com sucesso!');
-});
+});*/
 
+require('./database');
 
 const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
