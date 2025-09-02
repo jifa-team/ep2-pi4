5446a9fa803368e6c7963b9cc3411b6817cd871b
cadullira <cadullira@gmail.com>
2025-09-02 05:06:05 -0300

    refactor(ui): remove imagem de perfil do componente UserProfile

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -2,18 +2,13 @@
 
 import React from 'react';
 
-function UserProfile({ userName, profileImage }) {
+function UserProfile({ userName }) {
   return (
     <section className="flex items-center gap-6 mb-8">
       <div className="flex flex-col justify-center">
         <p className="text-muted-foreground">Bem vindo!</p>
         <strong className="text-xl text-primary">{userName}</strong>
       </div>
-      <img
-        className="w-20 h-20 rounded-full object-cover border-2 border-primary"
-        src={profileImage}
-        alt="foto do perfil do usuário"
-      />
     </section>
   );
 }
